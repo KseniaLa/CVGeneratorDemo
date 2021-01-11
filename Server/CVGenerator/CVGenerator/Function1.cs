ebaf6b72b94316ec188bd8ceb1a5249542ceca29
KseniaLa <ksla1998@mail.ru>
2021-01-11 11:09:17 +0300

    add docx file generation

--- Server/CVGenerator/CVGenerator/Function1.cs
+++ Server/CVGenerator/CVGenerator/Function1.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SelectPdf;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace CVGenerator
 {
@@ -19,34 +22,68 @@ namespace CVGenerator
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-               var htmlString = @"<html>
- <body>
-  <h1>Hello World from selectpdf.com</h1>
- </body>
-</html>
-";
 
-               HtmlToPdf converter = new HtmlToPdf();
-
-               // set converter options
-               converter.Options.PdfPageSize = PdfPageSize.A4;
-               converter.Options.PdfPageOrientation = PdfPageOrientation.Portrait;
-               converter.Options.WebPageWidth = 100;
-               converter.Options.WebPageHeight = 100;
-
-               // create a new pdf document converting an url
-               PdfDocument doc = converter.ConvertHtmlString(htmlString);
-
-               // save pdf document
-               var bytes = doc.Save();
-
-               // close pdf document
-               doc.Close();
-
-               return new FileContentResult(bytes, "application/octet-stream")
+               try
                {
-                    FileDownloadName = "file.pdf"
-               };
+                    //                    var htmlString = @"<html>
+                    // <body>
+                    //  <h1>Hello World from selectpdf.com</h1>
+                    // </body>
+                    //</html>
+                    //";
+
+                    //                    HtmlToPdf converter = new HtmlToPdf();
+
+                    //                    // set converter options
+                    //                    converter.Options.PdfPageSize = PdfPageSize.A4;
+                    //                    converter.Options.PdfPageOrientation = PdfPageOrientation.Portrait;
+                    //                    converter.Options.WebPageWidth = 100;
+                    //                    converter.Options.WebPageHeight = 100;
+
+                    //                    // create a new pdf document converting an url
+                    //                    PdfDocument doc = converter.ConvertHtmlString(htmlString);
+
+                    //                    // save pdf document
+                    //                    var bytes = doc.Save();
+
+                    //                    // close pdf document
+                    //                    doc.Close();
+
+                    using (MemoryStream mem = new MemoryStream())
+                    {
+                         using (WordprocessingDocument wordDocument =
+        WordprocessingDocument.Create(mem, WordprocessingDocumentType.Document, true))
+                         {
+                              MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                              // Create the document structure and add some text.
+                              mainPart.Document = new Document();
+                              Body body = mainPart.Document.AppendChild(new Body());
+                              Paragraph para = body.AppendChild(new Paragraph());
+                              Run run = para.AppendChild(new Run());
+                              run.AppendChild(new Text("Hello world!!!!!!"));
+                              mainPart.Document.Save();
+                              wordDocument.Close();
+
+                             
+                         }
+                         mem.Seek(0, SeekOrigin.Begin);
+                         return new FileContentResult(mem.ToArray(), "application/octet-stream")
+                         {
+                              FileDownloadName = "file.docx"
+                         };
+                    }
+
+                    
+               }
+               catch(Exception ex)
+               {
+                    log.LogError(ex.ToString());
+                    return new FileContentResult(new byte[] { }, "application/octet-stream")
+                    {
+                         FileDownloadName = "file.pdf"
+                    };
+               }
           }
     }
 }
